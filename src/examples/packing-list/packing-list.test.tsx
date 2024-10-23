ac7d8fd87172d7548a9c44cf6b866349db55ba88
Tayo Akosile <oluwatayoakosile@gmail.com>
2024-10-23 10:12:47 -0700

    WIP:adding test to packing list

--- src/examples/packing-list/packing-list.test.tsx
+++ src/examples/packing-list/packing-list.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from 'test/utilities';
+import { fireEvent, render, screen, waitFor } from 'test/utilities';
 import PackingList from '.';
 
 it('renders the Packing List application', () => {
@@ -14,25 +14,34 @@ it.todo('has an input field for a new item', () => {
   render(<PackingList />);
 });
 
-it('has a "Add New Item" button that is disabled when the input is empty', async () => {
 
+it('has a "Add New Item" button that is disabled when the input is empty', async () => {
+  render(<PackingList />);
+  const addNewItemInput = screen.getByRole('searchbox');
+  expect(addNewItemInput).toHaveValue('');
+  expect(screen.getByRole('button', { name: 'Add New Item' })).toBeDisabled();
+});
+
+it('enables the "Add New Item" button when there is text in the input field', async () => {
   const { user } = render(<PackingList />);
   const addNewItemInput = screen.getByRole('searchbox');
 
-  expect(addNewItemInput).toBeEmptyDOMElement();
-  expect(screen.getByRole('button', { name: 'Add New Item' })).toBeDisabled();
-
   await user.type(addNewItemInput, 'Apple');
-  expect(screen.getByRole('button', { name: 'Add New Item' })).toBeEnabled();
 
+  expect(addNewItemInput).toHaveValue('Apple');
+  expect(screen.getByRole('button', { name: 'Add New Item' })).toBeEnabled();
 });
 
-it.todo(
-  'enables the "Add New Item" button when there is text in the input field',
-  async () => {},
-);
+it('adds a new item to the unpacked item list when the clicking "Add New Item"', async () => {
+  const { user } = render(<PackingList />);
+  const getAddNewItemInputField = screen.getByRole('searchbox');
+  const getAddNewItemButton = screen.getByRole('button', {
+    name: 'Add New Item',
+  });
 
-it.todo(
-  'adds a new item to the unpacked item list when the clicking "Add New Item"',
-  async () => {},
-);
+  await user.type(getAddNewItemInputField, 'Apple');
+  await user.click(getAddNewItemButton);
+
+  // waitFor(()=>)
+  expect(screen.getAllByLabelText('Apple')).toBeDefined();
+});
